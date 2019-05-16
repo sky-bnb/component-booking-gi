bb1104badd2b2b794cbee5f8aff73036cab04e13
Gregory <gregoryinouye@gmail.com>
2019-05-16 14:29:11 -0700

    adding setup files and folders for client, server, database

---
+++ database/index.js
@@ -0,0 +1,25 @@
+// const mongoose = require('mongoose');
+// const db = mongoose.connection;
+
+// mongoose.connect('mongodb://localhost/skybnb-booking');
+// db.on('error', console.error.bind(console, 'connection error: '));
+// db.once('open', () => console.log('Connected to MongoDB'));
+
+// const bookingSchema = new mongoose.Schema({
+//   _id: Number,
+//   ownerName: String,
+//   guestMax: Number,
+//   price: Number,
+//   cleaningFee: Number,
+//   occupancyFee: Number,
+//   avgRating: Number,
+//   numReviews: Number,
+//   numViews: Number,
+//   minStay: Number,
+//   maxStay: Number,
+//   available: [Date]
+// });
+
+// const bookingModel = mongoose.model('bookingModel', bookingSchema);
+
+// export default bookingModel;
