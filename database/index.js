77444e107b544680e3e2d207237645b200579254
Gregory <gregoryinouye@gmail.com>
2019-05-17 15:59:44 -0700

    add server response to API call

--- database/index.js
+++ database/index.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
+const Booking = require('./schema.js');
 
 const db = mongoose.connection;
 const mongoDbName = 'mongodb://localhost/booking';
 
-// mongoose.connect(mongoLocation, {useNewUrlParser: true});
-// db.on('error', console.error.bind(console, 'connection error: '));
-// db.once('open', () => console.log('Connected to MongoDB Skybnb Booking'));
+mongoose.connect(mongoDbName, { useNewUrlParser: true });
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', () => console.log('Connected to MongoDB Skybnb Booking'));
 
-module.exports = { db, mongoDbName };
+const getBooking = (_id, callback) => Booking.find({ _id }, callback);
+
+module.exports = { db, mongoDbName, getBooking };
--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const { getBooking } = require('../database/index.js');
 
 const server = express();
 const port = 3004;
 
-server.get('/', (req, res) => res.send('Hello World!'));
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+server.use(express.static(__dirname + '/../public'));
+
+server.get('/host', (req, res) => {
+  getBooking(req.body._id, (err, collection) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.status(200).send(collection);
+    }
+  });
+});
 
 server.listen(port, () => console.log(`Booking Express Server listening on port ${port}`));
