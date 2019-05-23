424057e1a0d515635ae5da0b6c4fbbd0df8467e3
Gregory <gregoryinouye@gmail.com>
2019-05-23 10:53:33 -0700

    adds buttons to dropdown

--- src/components/GuestsDropdown.jsx
+++ src/components/GuestsDropdown.jsx
@@ -17,37 +17,77 @@ const StyledGuestsMenuWrapper = styled.div`
   margin: 16px 0px 16px;
 `;
 
-const StyledAdultWrapper = styled.div`
+const StyledAdultRow = styled.div`
+  margin: 16px 0px 16px 0px;
+  display: flex;
+  align-items: center;
 `;
 
-const StyledChildWrapper = styled.div`
+const StyledChildrenRow = styled.div`
+  margin: 24px 0px 24px 0px;
+  display: flex;
+  align-items: center;
 `;
 
-const StyledInfantWrapper = styled.div`
+const StyledInfantRow = styled.div`
+  margin: 24px 0px 24px 0px;
+  display: flex;
+  align-items: center;
 `;
 
 const StyledClosingWrapper = styled.div`
+  margin: 16px 0px 16px 0px;
 `;
 
-const StyledAdultsText = styled.div`
+const StyledMenuText = styled.div`
+  font-family: 'Quicksand', sans-serif;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 22px;
+  width: 176px;
 `;
 
-const StyledChildsText = styled.div`
+const StyledSubMenuText = styled.div`
+  font-family: 'Quicksand', sans-serif;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 18px;
 `;
 
-const StyledInfantsText = styled.div`
+const StyledTextWrapper = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 const StyledGuestMaxText = styled.div`
+  margin: 0px 0px 16px;
 `;
+
+const StyledMinusCounterPlusWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 120px;
+`;
+
+const StyledButton = styled.button`
+  background: transparent;
+  border-color: rgb(0, 132, 137);
+  border-radius: 50%;
+  border-width: 1px;
+  padding: 1px 7px 2px 7px;
+  cursor: pointer;
+  height: 32px;
+  width: 32px;
+`;
+
+const StyledSVG = styled.svg`
+  height: 1.5em;
+  width: 1.5em;
+  fill: rgb(0, 132, 137);
+`
 
 const StyledCloseButton = styled.button`
-`;
-
-const StyledPlusButton = styled.button`
-`;
-
-const StyledMinusButton = styled.button`
 `;
 
 const StyledCounterText = styled.div`
@@ -63,17 +103,54 @@ const StyledCounterText = styled.div`
 const GuestsDropdown = (props) => (
   <StyledGuestsDropdown>
     <StyledGuestsMenuWrapper>
-      <StyledAdultWrapper>
-
-      </StyledAdultWrapper>
-      <StyledChildWrapper>
-
-      </StyledChildWrapper>
-      <StyledInfantWrapper>
-
-      </StyledInfantWrapper>
+      <StyledAdultRow>
+        <StyledMenuText>Adults</StyledMenuText>
+        <StyledMinusCounterPlusWrapper>
+          <StyledButton>
+            <StyledSVG viewBox="0 0 24 24" role="img" focusable="false"><rect height="2" rx="1" width="12" x="6" y="11"></rect></StyledSVG>
+          </StyledButton>
+          <StyledCounterText>{props.numAdults}1</StyledCounterText>
+          <StyledButton>
+             <StyledSVG viewBox="0 0 24 24" role="img" focusable="false" ><rect height="2" rx="1" width="12" x="6" y="11" />
+            <rect height="12" rx="1" width="2" x="11" y="6" /></StyledSVG>
+          </StyledButton>
+        </StyledMinusCounterPlusWrapper>
+      </StyledAdultRow>
+      <StyledChildrenRow>
+        <StyledTextWrapper>
+          <StyledMenuText>Children</StyledMenuText>
+          <StyledSubMenuText>Ages 2-12</StyledSubMenuText>
+        </StyledTextWrapper>
+        <StyledMinusCounterPlusWrapper>
+          <StyledButton>
+              <StyledSVG viewBox="0 0 24 24" role="img" focusable="false"><rect height="2" rx="1" width="12" x="6" y="11"></rect></StyledSVG>
+            </StyledButton>
+            <StyledCounterText>{props.numChildren}0</StyledCounterText>
+            <StyledButton>
+              <StyledSVG viewBox="0 0 24 24" role="img" focusable="false" ><rect height="2" rx="1" width="12" x="6" y="11" />
+              <rect height="12" rx="1" width="2" x="11" y="6" /></StyledSVG>
+          </StyledButton>
+        </StyledMinusCounterPlusWrapper>
+      </StyledChildrenRow>
+      <StyledInfantRow>
+        <StyledTextWrapper>
+          <StyledMenuText>Infants</StyledMenuText>
+          <StyledSubMenuText>Under 2</StyledSubMenuText>
+        </StyledTextWrapper>
+        <StyledMinusCounterPlusWrapper>
+          <StyledButton>
+              <StyledSVG viewBox="0 0 24 24" role="img" focusable="false"><rect height="2" rx="1" width="12" x="6" y="11"></rect></StyledSVG>
+            </StyledButton>
+            <StyledCounterText>{props.numInfants}0</StyledCounterText>
+            <StyledButton>
+              <StyledSVG viewBox="0 0 24 24" role="img" focusable="false" ><rect height="2" rx="1" width="12" x="6" y="11" />
+              <rect height="12" rx="1" width="2" x="11" y="6" /></StyledSVG>
+          </StyledButton>
+        </StyledMinusCounterPlusWrapper>
+      </StyledInfantRow>
       <StyledClosingWrapper>
-
+        <StyledGuestMaxText>2 guests maximum. Infants don't count toward the number of guests.</StyledGuestMaxText>
+        <StyledCloseButton>Close</StyledCloseButton>
       </StyledClosingWrapper>
     </StyledGuestsMenuWrapper>
   </StyledGuestsDropdown>
