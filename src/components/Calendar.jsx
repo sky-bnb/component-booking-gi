85ba07164e6e037de1b1bc027d3231db644dac36
Gregory <gregoryinouye@gmail.com>
2019-05-24 18:34:50 -0700

    adds calendar rendering for any given month

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -6,28 +6,50 @@ import Day from './Day.jsx';
 const StyledCalendar = styled.table`
 `;
 
+// change size of calendar based on number of rendered weeks?
+
 const StyledHeader = styled.th`
 `;
 
-const StyledHeaderCells = styled.td`
-`;
+
+const buildMonth = dateObj => {
+  const firstDayOfMonth = dateObj.startOf('month').format('d');
+  const daysInMonth = dateObj.endOf('month').format('D');
+  const daysArray = [];
+  const monthArray = [];
+  const empty = { date: null, isBlank: true };
+
+  for (let i = 0; i < firstDayOfMonth; i += 1) {
+    daysArray.push(empty);
+  }
+
+  for (let i = 1; i <= daysInMonth; i += 1) {
+    daysArray.push({ date: i, isBlank: false });
+  }
+  
+  const daysToPad = 7 - (daysArray.length % 7);
+
+  for (let i = 0; i < daysToPad; i += 1) {
+    daysArray.push(empty);
+  }
+
+  for (let i = 0; i < daysArray.length / 7; i += 1) {
+    const week = daysArray.slice(i * 7, (i + 1) * 7);
+    monthArray.push(week);
+  }
+
+  return monthArray;
+}
+
+const month = buildMonth(moment());
 
 const Calendar = () => {
-  const arr = [];
-
-  for (let i = 0; i < 6; i += 1) {
-    const inner = [];
-    for (let j = 0; j < 7; j += 1) {
-      inner.push(j + i * 7);
-    }
-    arr.push(inner);
-  }
 
   return (
     <StyledCalendar>
       <caption>CALENDAR</caption>
-      <thead><tr><th>Su</th><th>Mo</th><th>Tu</th><th>We</th><th>Th</th><th>Fr</th><th>Sa</th></tr></thead>
-      <tbody>{arr.map(element => <tr>{ element.map(element => <Day date={element} key={element} isBlank={false} />) }</tr> )}</tbody>
+      <thead><tr><StyledHeader>Su</StyledHeader><StyledHeader>Mo</StyledHeader><StyledHeader>Tu</StyledHeader><StyledHeader>We</StyledHeader><StyledHeader>StyledHeader</StyledHeader><StyledHeader>Fr</StyledHeader><StyledHeader>Sa</StyledHeader></tr></thead>
+      <tbody>{month.map(week => <tr>{ week.map(({ date, isBlank}, index) => <Day date={date} key={index} isBlank={isBlank} />) }</tr> )}</tbody>
     </StyledCalendar>
   );
 };
