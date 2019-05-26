d47cfedc2eef0304a3c08bc73e57284efffe3fa2
Gregory <gregoryinouye@gmail.com>
2019-05-26 16:26:23 -0700

    add neon blue highlight to date field when checkin/checkout field is selected

--- src/components/Dates.jsx
+++ src/components/Dates.jsx
@@ -21,6 +21,7 @@ const StyledDateLabel = styled.label`
 
 const StyledDatesBox = styled.div`
   display: table;
+  table-layout: fixed;
   height: 40px;
   width: 100%;
   border-width: 1px;
@@ -31,67 +32,56 @@ const StyledDatesBox = styled.div`
 
 const StyledDatesRow = styled.div`
   display: table-row;
-  table-layout: fixed;
   width: 100%;
-  height: 40px;
+  height: 100%
 `;
 
-const StyledDateField = styled.div`
+const StyledDateCell = styled.div`
   display: table-cell;
   position: relative;
-  font-weight: 400;
-  font-size: 17px;
-  color: rgb(117, 117, 117);
+  width: 100%;
+  height: 100%;
   vertical-align: middle;
-  margin: 0px;
-  padding: 8px 14px 8px 14px;
-  background: rgb(255, 255, 255);
-  width: auto;
 `;
 
 const StyledDateInput = styled.input`
   position: absolute;
   top: 0;
   left: 0;
-  font-size: 16px;
-  font-weight: 400;
   opacity: 0;
   height: 100%;
   width: 100%;
   border-width: 0px;
-  border-style: initial;
-  border-color: initial;
-  border-image: initial;
-  text-rendering: auto;
-  color: initial;
-  letter-spacing: normal;
-  word-spacing: normal;
-  text-transform: none;
-  text-indent: 0px;
-  text-shadow: none;
-  text-align: start;
-  margin: 0em;
-  font: 400 11px system-ui;
-  padding: 1px;
+  box-sizing: border-box;
 `;
 
 const StyledHighlight = styled.div`
+  font-size: 17px;
+  color: rgb(72, 72, 72);
   position: absolute;
+  pointer-events: none;
   top: 0;
-  left: 0:
+  left: 0;
+  right: 0;
+  bottom: 0;
+  box-sizing: border-box;
   padding: 0px 6px 0px 6px;
+  margin: 8px 8px 8px 8px;
   border-radius: 3px;
   overflow: hidden;
-  white-space: nowrap;
-  background: ${({ isCheckinOpen }) => isCheckinOpen ? 'rgb(153, 237, 230)' : null};
-  border-color: ${({ isCheckinOpen }) => isCheckinOpen ? 'rgb(153, 237, 230)' : null};
+  background: ${({ isCheckinOpen, isCheckoutOpen }) => isCheckinOpen || isCheckoutOpen ? 'rgb(153, 237, 230)' : 'white'};
+  border-color: ${({ isCheckinOpen, isCheckoutOpen }) => isCheckinOpen || isCheckoutOpen ? 'rgb(153, 237, 230)' : 'white'};
+`;
+
+const StyledSVGWrapper = styled.div`
+  display: table-cell;
+  width: 24px;
 `;
 
 const StyledSVG = styled.svg`
-  display: table-cell;
   height: 24px;
   width: 24px;
-  padding: 8px 0px 8px 0px;
+  padding: 6px 0px 6px 0px;
   margin: 0px;
 `;
 
@@ -106,19 +96,19 @@ const Dates = (props) => {
       <StyledDateLabel>Dates</StyledDateLabel>
       <StyledDatesBox>
         <StyledDatesRow>
-          <StyledDateField>
-            {checkinValue}
-            <StyledHighlight isCheckinOpen={isCheckinOpen} />
+          <StyledDateCell>
             <StyledDateInput type="text" placeholder="Check-in" value={checkinValue} onChange={updateCheckinDate} onClick={toggleCheckinDropdown} />
-          </StyledDateField>
-          <StyledSVG viewBox="0 0 24 24" role="presentation" focusable="false">
-            <path d="m0 12.5a.5.5 0 0 0 .5.5h21.79l-6.15 6.15a.5.5 0 1 0 .71.71l7-7v-.01a.5.5 0 0 0 .14-.35.5.5 0 0 0 -.14-.35v-.01l-7-7a .5.5 0 0 0 -.71.71l6.15 6.15h-21.79a.5.5 0 0 0 -.5.5z" fillRule="evenodd" />
-          </StyledSVG>
-          <StyledDateField>
-            {checkoutValue}
-            <StyledHighlight isCheckoutOpen={isCheckoutOpen} />
+            <StyledHighlight isCheckinOpen={isCheckinOpen}>{checkinValue !== '' ? checkinValue : 'Check-in'}</StyledHighlight>
+          </StyledDateCell>
+          <StyledSVGWrapper>
+            <StyledSVG viewBox="0 0 24 24" role="presentation" focusable="false">
+              <path d="m0 12.5a.5.5 0 0 0 .5.5h21.79l-6.15 6.15a.5.5 0 1 0 .71.71l7-7v-.01a.5.5 0 0 0 .14-.35.5.5 0 0 0 -.14-.35v-.01l-7-7a .5.5 0 0 0 -.71.71l6.15 6.15h-21.79a.5.5 0 0 0 -.5.5z" fillRule="evenodd" />
+            </StyledSVG>
+          </StyledSVGWrapper>
+          <StyledDateCell>
             <StyledDateInput type="text" placeholder="Checkout" value={checkoutValue} onChange={updateCheckoutDate} onClick={toggleCheckoutDropdown} />
-          </StyledDateField>
+            <StyledHighlight isCheckoutOpen={isCheckoutOpen}>{checkoutValue !== '' ? checkoutValue : 'Checkout'}</StyledHighlight>
+          </StyledDateCell>
         </StyledDatesRow>
       </StyledDatesBox>
       {isCheckinOpen || isCheckoutOpen ? <DatesDropdown
